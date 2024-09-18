bc922dec7e7d32880e40892bc569f9f67249631e
Devananda Jyothi <devanandajyothi2005@gmail.com>
2024-09-18 21:33:07 +0530

    added env

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 
 function App() {
-    const CLIENT_ID = "010841a0f9904a25b4789f977225c34e";
-    const REDIRECT_URI = "http://localhost:5173";
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-    const RESPONSE_TYPE = "token";
-    const SCOPES = "user-top-read";
+    const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+    const REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+    const AUTH_ENDPOINT = import.meta.env.VITE_SPOTIFY_AUTH_ENDPOINT;
+    const RESPONSE_TYPE = import.meta.env.VITE_SPOTIFY_RESPONSE_TYPE;
+    const SCOPES = import.meta.env.VITE_SPOTIFY_SCOPES;
+
+
     const [token, setToken] = useState("");
     const [data, setData] = useState([]); // For both tracks and artists
     const [type, setType] = useState('tracks'); // Default type is tracks
@@ -74,17 +76,20 @@ function App() {
         if (loading) {
             return <p className="text-white">Loading...</p>;
         }
-
+    
         if (error) {
             return <p className="text-red-500">{error}</p>;
         }
-
+    
         if (data.length === 0) {
             return <p className="text-white text-center">Nothing to display</p>;
         }
-
-        return data.map(item => (
+    
+        return data.map((item, index) => (
             <div key={item.id} className="flex items-center bg-gray-800 p-4 rounded-lg shadow-lg mb-4 max-w-lg mx-auto">
+                <div className="flex-shrink-0 w-10 h-10 flex items-center justify-center text-white font-bold bg-gray-700 rounded-full mr-4">
+                    {index + 1}
+                </div>
                 <img
                     src={type === 'tracks' ? item.album.images[0]?.url : item.images[0]?.url}
                     alt={item.name}
@@ -108,6 +113,7 @@ function App() {
             </div>
         ));
     };
+    
 
     return (
         <div className="bg-gray-900 min-h-screen text-gray-200">
